ae5d82959e5e405f3d2a88cbcf8c97d30e680c19
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-07-04 13:16:42 +0200

    Exercise 15
    Add a comment with a source that was used for the solution.

--- ex15.rkt
+++ ex15.rkt
@@ -1,6 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname ex15) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; See https://en.wikipedia.org/wiki/Material_conditional for hints.
+
 (define (==> a b)
   (or (not a) b))
 
