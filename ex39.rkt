f244b01a24d8823f7a8e8960099c8445fad5f8bf
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-07-04 18:52:47 +0200

    Exercise 39

---
+++ ex39.rkt
@@ -0,0 +1,41 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex39) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+; PHYSICAL CONSTANTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(define WIDTH-OF-WORLD 200)
+
+; ENTRY POINT
+(define WHEEL-RADIUS 5)
+
+(define WHEEL-DISTANCE (* WHEEL-RADIUS 5))
+
+; GRAPHICAL CONSTANTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(define WHEEL
+  (circle WHEEL-RADIUS "solid" "black"))
+
+(define SPACE
+  (rectangle WHEEL-DISTANCE 0 "solid" "white"))
+
+(define BOTH-WHEELS
+  (beside WHEEL SPACE WHEEL))
+
+(define CHASSIS
+  (rectangle (* WHEEL-RADIUS 11) (* 2 WHEEL-RADIUS) "solid" "red"))
+(define CABIN
+  (rectangle (+ (image-width SPACE) (* 2 WHEEL-RADIUS))
+             (* 2 WHEEL-RADIUS)
+             "solid" "red"))
+
+(define CAR (overlay/align/offset
+             "middle"
+             "bottom"
+             BOTH-WHEELS
+             0
+            (* -1 WHEEL-RADIUS)
+             (above CABIN CHASSIS)))
+
+CAR
