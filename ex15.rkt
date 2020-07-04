b44edb49efcd51868059d0633432d30d6f0226eb
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-07-04 13:11:14 +0200

    Exercise 15

---
+++ ex15.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex15) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (==> a b)
+  (or (not a) b))
+
+(==> #true #true)      ; true
+(==> #true #false)     ; false
+(==> #false #true)     ; true
+(==> #false #false)    ; true
