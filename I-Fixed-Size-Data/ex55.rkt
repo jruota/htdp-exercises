452497794c242c1dd4a97153d6fe2cc0577da765
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-07-11 11:02:55 +0200

    Exercise 55

---
+++ I-Fixed-Size-Data/ex55.rkt
@@ -0,0 +1,92 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex55) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; An LRCD (for launching rocket countdown) is one of:
+; – "resting"
+; – a Number between -3 and -1
+; – a NonnegativeNumber 
+; interpretation a grounded rocket, in countdown mode,
+; a number denotes the number of pixels between the
+; top of the canvas and the rocket (its height)
+
+; DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(define HEIGHT 300) ; distances in pixels 
+(define WIDTH  100)
+(define YDELTA 3)
+ 
+(define BACKG  (empty-scene WIDTH HEIGHT))
+(define ROCKET (rectangle 5 30 "solid" "red"))
+ 
+(define CENTER (/ (image-height ROCKET) 2))
+
+; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; Why would it be incorrect to use (string=? "resting" x) as the first
+; condition in show? Conversely, formulate a completely accurate condition,
+; that is, a Boolean expression that evaluates to #true precisely when x
+; belongs to the first sub-class of LRCD.
+
+; LRCD can be a String or a Number but string=? always expects a String.
+; So when show is passed a Number, (string=?  "resting" x) would result
+; in an error.
+
+; LRCD -> Image
+; renders the state as a resting or flying rocket 
+(define (show x)
+  (cond
+    [(string? x)
+     (place-rocket (- HEIGHT CENTER))]
+    [(<= -3 x -1)
+     (place-image (text (number->string x) 20 "red")
+                  10
+                  (* 3/4 WIDTH)
+                  (place-rocket (- HEIGHT CENTER)))]
+    [(>= x 0)
+     (place-rocket (- x CENTER))]))
+
+(check-expect
+ (show "resting")
+ (place-image ROCKET 10 (- HEIGHT CENTER) BACKG))
+
+(check-expect
+ (show -2)
+ (place-image (text "-2" 20 "red")
+              10 (* 3/4 WIDTH)
+              (place-image ROCKET 10 (- HEIGHT CENTER) BACKG)))
+ 
+(check-expect
+ (show 53)
+ (place-image ROCKET 10 (- 53 CENTER) BACKG))
+
+(check-expect
+ (show HEIGHT)
+ (place-image ROCKET 10 (- HEIGHT CENTER) BACKG))
+
+(check-expect (show 0)
+              (place-image ROCKET 10 (- 0 CENTER) BACKG))
+
+; Number -> Image
+; Place the rocket at y in BACKG.
+(define (place-rocket y)
+  (place-image ROCKET 10 y BACKG))
+
+(check-expect (place-rocket 100)
+              (place-image ROCKET 10 100 BACKG))
+ 
+; LRCD KeyEvent -> LRCD
+; starts the countdown when space bar is pressed, 
+; if the rocket is still resting 
+(define (launch x ke)
+  x)
+ 
+; LRCD -> LRCD
+; raises the rocket by YDELTA,
+; if it is moving already 
+(define (fly x)
+  x)
