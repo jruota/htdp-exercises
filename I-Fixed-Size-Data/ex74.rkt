45b77d037cc835d818f7eb901755120a1cf3c973
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-07-12 17:54:11 +0200

    Exercise 74

---
+++ I-Fixed-Size-Data/ex74.rkt
@@ -0,0 +1,64 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex74) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/universe)
+(require 2htdp/image)
+
+; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; A Posn represents the state of the world.
+
+; DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(define MTS (empty-scene 100 100))
+(define DOT (circle 3 "solid" "red"))
+
+; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; Posn -> Posn 
+(define (main p0)
+  (big-bang p0
+    [on-tick x+]
+    [on-mouse reset-dot]
+    [to-draw scene+dot]))
+
+; Posn -> Image
+; adds a red spot to MTS at p
+(define (scene+dot p)
+  (place-image DOT (posn-x p) (posn-y p) MTS))
+
+(check-expect (scene+dot (make-posn 10 20))
+              (place-image DOT 10 20 MTS))
+(check-expect (scene+dot (make-posn 88 73))
+              (place-image DOT 88 73 MTS))
+
+; Posn -> Posn
+; increases the x-coordinate of p by 3
+(define (x+ p)
+  (posn-up-x p (+ (posn-x p) 3)))
+
+(check-expect (x+ (make-posn 10 0))
+              (make-posn 13 0))
+
+; from ex73.rkt
+; Posn Number -> Posn
+; Produce a Posn like p with n in the x field.
+(define (posn-up-x p n)
+  (make-posn n (posn-y p)))
+
+(check-expect (posn-up-x (make-posn 0 0) 1)
+              (make-posn 1 0))
+
+; Posn Number Number MouseEvt -> Posn 
+; for mouse clicks, (make-posn x y); otherwise p
+(define (reset-dot p x y me)
+  (cond
+    [(mouse=? me "button-down") (make-posn x y)]
+    [else p]))
+
+(check-expect
+  (reset-dot (make-posn 10 20) 29 31 "button-down")
+  (make-posn 29 31))
+(check-expect
+  (reset-dot (make-posn 10 20) 29 31 "button-up")
+  (make-posn 10 20))
