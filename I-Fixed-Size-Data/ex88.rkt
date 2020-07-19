6db6e34b2fcd1015c2fa98fe22dd07327828b479
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-07-19 18:37:18 +0200

    Exercise 88

---
+++ I-Fixed-Size-Data/ex88.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex88) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct vcat [x hap])
+; A VCat is a structure:
+;     (make-vcat Number Number)
+; where the following condition applys:
+;     – hap is a number greater or equal to 0.
+; Interpretation:
+;     The cat's current x-coordinate
+;     and its happiness level.
