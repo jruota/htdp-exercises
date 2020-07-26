2900416ee1bc5ea743c79a3b6fb8af03ea696ebc
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-07-26 12:48:59 +0200

    Exercise 129

---
+++ II-Arbitrarily-Large-Data/ex129.rkt
@@ -0,0 +1,36 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex129) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; https://en.wikipedia.org/wiki/Solar_System
+
+(cons "Sun"
+      (cons "Mercury"
+            (cons "Venus"
+                  (cons "Earth"
+                        (cons "Mars"
+                              (cons "Jupiter"
+                                    (cons "Saturn"
+                                          (cons "Uranus"
+                                                (cons "Neptune" '())))))))))
+
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(cons "steak"
+      (cons "french fries"
+            (cons "beans"
+                  (cons "bread"
+                        (cons "water"
+                              (cons "Brie cheese"
+                                    (cons "ice cream" '())))))))
+
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(cons "red"
+      (cons "white"
+            (cons "yellow"
+                  (cons "black"
+                        (cons "blue"
+                              (cons "green"
+                                    (cons "orange"
+                                          (cons "purple"
+                                                (cons "brown" '())))))))))
