f1089248813be0f47459edb2927ecb4251317c33
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-07-26 17:37:45 +0200

    Exercise 131

---
+++ II-Arbitrarily-Large-Data/ex131.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex131) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; A List-of-Booleans is one of:
+; – '()
+; – (cons List-of-Booleans '())
+; Interpretation:
+;     All lists containing Boolean values
+;     only and of arbitrary length.
