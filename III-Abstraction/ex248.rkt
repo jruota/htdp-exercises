2d0c868d0f387adefe334d1fb6cdbde790c00b09
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-11-28 12:26:56 +0100

    Exercise 248

---
+++ III-Abstraction/ex248.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex248) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Number Number -> Boolean
+; Is the area of a square with side x larger than c?
+(define (squared>? x c)
+  (> (* x x) c))
+
+(check-expect (squared>? 2 4)
+              #false)
+(check-expect (squared>? 2 3)
+              #true)
+
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(squared>? 3 10)
+(squared>? 4 10)
