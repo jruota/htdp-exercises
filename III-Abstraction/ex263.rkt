7c4ae16bb6caf577973e38fc16f93e349a4e075f
Jaroław Kołosowski <jarokolosowski@gmail.com>
2020-12-21 09:35:19 +0100

    Exercise 263

---
+++ III-Abstraction/ex263.rkt
@@ -0,0 +1,26 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname ex263) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; A Nelon (non-empty list of numbers)
+; is one of:
+;     – (list Number)
+;     – (cons Number Nelon)
+; Interpretation:
+;     A non-empty list of numbers.
+
+; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; Nelon -> Number
+; determines the smallest number on l
+(define (inf.v2 l)
+  (cond
+    [(empty? (rest l)) (first l)]
+    [else
+     (local ((define smallest-in-rest (inf.v2 (rest l))))
+       (if (< (first l) smallest-in-rest)
+           (first l)
+           smallest-in-rest))]))
+
+(inf.v2 (list 2 1 3))
