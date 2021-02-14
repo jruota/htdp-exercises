f04499e5cfc027aacc519b821da9c175cd8bc207
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-02-14 10:49:10 +0100

    Exercise 314

---
+++ IV-Intertwined-Data/ex314.rkt
@@ -0,0 +1,70 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex314) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(define-struct no-parent [])
+(define-struct child [father mother name date eyes])
+(define NP (make-no-parent))
+; An FT (short for family tree) is one of: 
+; – NP
+; – (make-child FT FT String N String)
+
+; An FF (short for family forest) is one of: 
+; – '()
+; – (cons FT FF)
+; interpretation a family forest represents several
+; families (say, a town) and their ancestor trees
+
+;                      ||
+;                      \/
+
+; A [List-of X] is one of:
+; – '()
+; – (cons X [List-of X])
+
+; DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; Oldest Generation:
+(define Carl (make-child NP NP "Carl" 1926 "green"))
+(define Bettina (make-child NP NP "Bettina" 1926 "green"))
+ 
+; Middle Generation:
+(define Adam (make-child Carl Bettina "Adam" 1950 "hazel"))
+(define Dave (make-child Carl Bettina "Dave" 1955 "black"))
+(define Eva (make-child Carl Bettina "Eva" 1965 "blue"))
+(define Fred (make-child NP NP "Fred" 1966 "pink"))
+ 
+; Youngest Generation: 
+(define Gustav (make-child Fred Eva "Gustav" 1988 "brown"))
+
+; Family Forests
+(define ff1 (list Carl Bettina))
+(define ff2 (list Fred Eva))
+(define ff3 (list Fred Eva Carl))
+(define ff4 (list Gustav))
+
+; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; [List-of FT] -> Boolean
+; Does the forest contain any child with "blue" eyes?
+(define (blue-eyed-child-in-forest? a-forest)
+  (ormap blue-eyed-child? a-forest))
+
+(check-expect (blue-eyed-child-in-forest? ff1) #false)
+(check-expect (blue-eyed-child-in-forest? ff2) #true)
+(check-expect (blue-eyed-child-in-forest? ff3) #true)
+(check-expect (blue-eyed-child-in-forest? ff4) #true)
+
+; FT -> Boolean
+; Does an-ftree contain a child structure
+; with "blue" in the eyes field? 
+(define (blue-eyed-child? an-ftree)
+  (cond
+    [(no-parent? an-ftree) #false]
+    [else (or (string=? (child-eyes an-ftree) "blue")
+              (blue-eyed-child? (child-father an-ftree))
+              (blue-eyed-child? (child-mother an-ftree)))]))
+
+(check-expect (blue-eyed-child? Carl) #false)
+(check-expect (blue-eyed-child? Gustav) #true)
