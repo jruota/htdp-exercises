83dff30b3b429b81795e0de414f1e75b36c8769f
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-02-21 19:27:59 +0100

    Exercise 317

---
+++ IV-Intertwined-Data/ex317.rkt
@@ -0,0 +1,54 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex317) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; An S-expr is one of: 
+; – Atom
+; – SL
+ 
+; An SL is one of: 
+; – '()
+; – (cons S-expr SL)
+
+; An Atom is one of: 
+; – Number
+; – String
+; – Symbol 
+
+; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; S-expr Symbol -> N 
+; counts all occurrences of sy in sexp 
+(define (count sexp sy)
+  (local (; Any -> Boolean
+          ; Is a of type atom?
+          (define (atom? a)
+            (or (number? a)
+                (string? a)
+                (symbol? a)))
+
+          ; SL Symbol -> N
+          ; counts all occurrences of sy in sl
+          (define (count-sl sl)
+            (cond
+              [(empty? sl) 0]
+              [else
+               (+ (count (first sl) sy) (count-sl (rest sl)))]))
+
+          ; Atom Symbol -> N
+          ; counts all occurrences of sy in at
+          (define (count-atom at)
+            (cond
+              [(number? at) 0]
+              [(string? at) 0]
+              [(symbol? at) (if (symbol=? at sy) 1 0)])))
+    ; – IN –
+    (cond
+      [(atom? sexp) (count-atom sexp)]
+      [else (count-sl sexp)])))
+
+(check-expect (count 'world 'hello) 0)
+(check-expect (count '(world hello) 'hello) 1)
+(check-expect (count '(((world) hello) hello) 'hello) 2)
+ 
