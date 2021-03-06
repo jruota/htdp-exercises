e1bef12383f4c55dd30e25110b16e52c2f2e5a2c
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-03-06 12:18:23 +0100

    Exercise 318 alternative
    The empty list has a depth of 0 instead of 1 in this version.

---
+++ IV-Intertwined-Data/ex318-alt.rkt
@@ -0,0 +1,89 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex318-alt) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+; NOTE +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+; This is an alternative version of exercise 318 (ex318.rkt). The difference is
+; that here the empty list has a depth of 0, not 1.
+
+; To achieve this, only the result of the appropriate conditional clause in
+; "sl-depth" had to be changed from 1 to 0.
+
+; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; An S-expr is one of: 
+; – Atom
+; – SL
+ 
+; An SL is one of: 
+; – '()
+; – (cons S-expr SL)
+
+; An Atom is one of: 
+; – Number
+; – String
+; – Symbol 
+
+; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; S-expr -> N
+; Return the depth of sexpr.
+(define (depth sexpr)
+  (cond
+    [(atom? sexpr) 1]
+    [else
+     (sl-depth sexpr)]))
+
+(check-expect (depth 3.14)
+              1)
+(check-expect (depth "hello")
+              1)
+(check-expect (depth 'world)
+              1)
+(check-expect (depth '())
+              0)
+(check-expect (depth '(4 "banana" + ()))
+              2)
+(check-expect (depth '(((world) hello) hello))
+              4)
+(check-expect (depth '(4 "banana" + () (((world) hello) hello)))
+              5)
+
+; SL -> N
+; Return the depth of sl.
+(define (sl-depth sl)
+  (cond
+    [(empty? sl) 0]
+    [else
+     (max (+ 1 (depth (first sl)))
+          (sl-depth (rest sl)))]))
+
+(check-expect (sl-depth '())
+              0)
+(check-expect (sl-depth '(((world) hello) hello))
+              4)
+(check-expect (sl-depth (list '() '() '() '() '() '()))
+              1)
+
+; Any -> Boolean
+; Is a of type atom?
+(define (atom? a)
+  (or (number? a)
+      (string? a)
+      (symbol? a)))
+
+(check-expect (atom? 4.321)
+              #true)
+(check-expect (atom? "hello")
+              #true)
+(check-expect (atom? '+)
+              #true)
+
+(check-expect (atom? (make-posn 1 2))
+              #false)
+(check-expect (atom? empty-image)
+              #false)
+(check-expect (atom? '())
+              #false)
