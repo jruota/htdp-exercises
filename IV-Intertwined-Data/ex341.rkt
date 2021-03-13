29ea6cc169ebd8ccfa1bac1413e0b2d2c1cec33e
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-03-13 20:09:39 +0100

    Exercise 341

---
+++ IV-Intertwined-Data/ex341.rkt
@@ -0,0 +1,43 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex341) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require htdp/dir)
+
+; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; (define-struct file [name size content])
+; A File is a structure: 
+;   (make-file String N String)
+
+; (define-struct dir [name dirs files])
+; A Dir is a structure: 
+;   (make-dir.v3 String [List-of Dir] [List-of File])
+
+; An N is one of:
+; – 0
+; – (add1 N)
+; Interpretation:
+;     The counting numbers,
+;     including 0.
+
+; DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(define TS
+  (create-dir
+   (string-append "/home/jruota/learning/cs/teach-yourself-cs/"
+                  "01-HtDP/htdp-exercises/IV-Intertwined-Data/TS")))
+
+; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; Dir -> N
+; Compute the total size of all the
+; files in the entire directory tree.
+(define (du dir)
+  (+ 1
+     (foldl (lambda (x init) (+ (file-size x) init))  0 (dir-files dir))
+     (foldl + 0 (map du (dir-dirs dir)))))
+
+(check-expect (du (make-dir "empty" '() '()))
+              1)
+(check-expect (du TS)
+              212)
