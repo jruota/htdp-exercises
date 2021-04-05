0ddbcb4b9ef48dd5f57764b1100db7546a2d07c0
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-04-05 10:12:21 +0200

    Exercise 340 – complete redesign
    Redesigned the function from the ground up according to the design
    recipe. Added another, more refined version based on that.

--- IV-Intertwined-Data/ex340.rkt
+++ IV-Intertwined-Data/ex340.rkt
@@ -13,13 +13,6 @@
 ; A Dir is a structure: 
 ;   (make-dir.v3 String [List-of Dir] [List-of File])
 
-; An N is one of:
-; – 0
-; – (add1 N)
-; Interpretation:
-;     The counting numbers,
-;     including 0.
-
 ; DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 (define TS
@@ -29,16 +22,66 @@
 
 ; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+; NOTE +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+; According to the design recipe for intertwined data, there should be four
+; functions (one for File, [List-of File], Dir and [List-of Dir] respectively).
+; The File function would only extract the file name, which is overkill for a
+; standalone function and can be easily achieved in the [List-of File] function.
+
+; END NOTE +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 ; Dir -> [List-of String]
 ; List the names of all files and directories in dir.
 (define (ls dir)
-  (sort
-   (append
-    (map (lambda (x) (dir-name x)) (dir-dirs dir))
-    (map (lambda (x) (file-name x)) (dir-files dir)))
-   string<?))
+  (local (; [List-of File] -> [List-of String]
+          ; Return all the names of the files
+          ; in lof.
+          (define (lof-ls lof)
+;            (cond
+;              [(empty? lof) '()]
+;              [else
+;               (cons (file-name (first lof))
+;                     (lof-ls (rest lof)))]))
+            (map file-name lof))
+
+          ; [List-of Dir] -> [List-of String]
+          ; Return all the names of the
+          ; directories in lod.
+          (define (lod-ls lod)
+;            (cond
+;              [(empty? lod) '()]
+;              [else
+;               (cons (dir-name (first lod))
+;                     (lod-ls (rest lod)))]))
+            (map dir-name lod))
+
+          ; Dir -> [List-of String]
+          ; List all the names of the
+          ; files and directories in d,
+          ; sorted alphabetically.
+          (define (dir-ls d)
+            (sort
+             (append (lof-ls (dir-files d))
+                     (lod-ls (dir-dirs d)))
+             string<?)))
+    ; – IN –
+    (dir-ls dir)))
 
 (check-expect (ls (make-dir "empty" '() '()))
               '())
 (check-expect (ls TS)
+              (sort (list "Libs" "read!" "Text") string<?))
+
+; Dir -> [List-of String]
+; List the names of all files and directories in dir.
+(define (ls.v2 dir)
+  (sort
+   (append (map file-name (dir-files dir))
+           (map dir-name (dir-dirs dir)))
+   string<?))
+
+(check-expect (ls.v2 (make-dir "empty" '() '()))
+              '())
+(check-expect (ls.v2 TS)
               (sort (list "Libs" "read!" "Text") string<?))
