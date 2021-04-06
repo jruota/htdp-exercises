c821d93814d1bf222cc5bd33558eb8f81ab7400b
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-04-06 20:01:45 +0200

    Exercise 342 – complete redesign
    Redesigned the function find from the ground up according to the
    design recipe.

--- IV-Intertwined-Data/ex342.rkt
+++ IV-Intertwined-Data/ex342.rkt
@@ -11,20 +11,19 @@
 
 ; (define-struct dir [name dirs files])
 ; A Dir is a structure: 
-;   (make-dir.v3 String [List-of Dir] [List-of File])
-
-; An N is one of:
-; – 0
-; – (add1 N)
-; Interpretation:
-;     The counting numbers,
-;     including 0.
+;   (make-dir String [List-of Dir] [List-of File])
 
 ; A PathOrFalse is one of:
 ; – #false
 ; – [List-of String]
 ; Interpretation:
-;     The path to a file or #false.
+;     The path to a file if it exists or #false.
+
+; A [X]OrFalse is one of:
+; – #false
+; – [X]
+; Interpretation:
+;     An X or #false.
 
 ; DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -39,24 +38,51 @@
 ; If file f is somewhere in directory dir,
 ; return the path to file f, #false otherwise.
 (define (find dir f)
-  (local (; [List-of File] -> Boolean
-          ; Does file occur in lof of the current working directory?
-          (define (file-in-current-dir? lof)
-            (ormap (lambda (x) (string=? f (file-name x))) lof)))
-    
-    ; – IN –
+  (local (; [List-of File] -> StringOrFalse
+          ; Return the name of file f
+          ; if it is in lof,
+          ; #false otherwise.
+          (define (lof-find lof)
+            (cond
+              [(empty? lof) #false]
+              [else
+               (if (string=? (file-name (first lof)) f)
+                   (file-find (first lof))
+                   (lof-find (rest lof)))]))
 
-    (if (find? dir f)
-        (if (file-in-current-dir? (dir-files dir))
-            (list (dir-name dir) f)
-            (cons (dir-name dir)
-                  (first
-                   (filter
-                    (lambda (x) (cons? x))
-                    (map
-                     (lambda (x) (find x f))
-                     (dir-dirs dir))))))
-        #false)))
+          ; [List-of Dir] -> PathOrFalse
+          ; If file f exists in one of the
+          ; directories in lod, return the
+          ; path to the file, #false elsewhere.
+          (define (lod-find lod)
+            (cond
+              [(empty? lod) #false]
+              [else
+               (if (find? (first lod) f)
+                   (dir-find (first lod))
+                   (lod-find (rest lod)))]))
+
+          ; File -> String
+          ; Return the file name of file f.
+          (define (file-find f)
+            (file-name f))
+
+          ; Dir -> PathOrFalse
+          ; If file f exists is somewhere in directory dir,
+          ; return the path to file f, #false otherwise.
+          (define (dir-find d)
+            (local ((define IN-SUBDIRS (lod-find (dir-dirs d))))
+              ; – IN –
+              (cond
+                [(not (false? (lof-find (dir-files d))))
+                 (list (dir-name d)
+                       f)]
+                [(not (false? IN-SUBDIRS))
+                 (cons (dir-name d)
+                       IN-SUBDIRS)]
+                [else #false]))))
+    ; – IN –
+    (dir-find dir)))
 
 (check-expect (find TS "part4")
               #false)
@@ -69,24 +95,13 @@
 (check-expect (find (first (dir-dirs TS)) "read!")
               (list "Libs" "Docs" "read!"))
 
+; from ex399.rkt +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 ; Dir String -> Boolean
 ; Does file occur in the directory tree dir?
 (define (find? dir file)
-  (local (; [List-of File] -> Boolean
-          ; Does file occur in lof of the current working directory?
-          (define (file-in-current-dir? lof)
-            (ormap (lambda (x) (string=? file (file-name x))) lof))
-
-          ; [List-of Dir] -> Boolean
-          ; Does file occur in any of the
-          ; directories listed in lod?
-          (define (file-in-subdirs? lod)
-            (ormap (lambda (x) (find? x file)) lod)))
-
-    ; – IN –
-
-    (or (file-in-current-dir? (dir-files dir))
-        (file-in-subdirs? (dir-dirs dir)))))
+  (or (ormap (lambda (x) (string=? (file-name x) file)) (dir-files dir))
+      (ormap (lambda (x) (find? x file)) (dir-dirs dir))))
 
 (check-expect (find? TS "read!")
               #true)
@@ -94,56 +109,3 @@
               #false)
 (check-expect (find? TS "hang")
               #true)
-
-;; Dir String -> Boolean
-;; Does file occur in the directory tree dir?
-;(define (find? dir file)
-;  (or (file-in-current-dir? (dir-files dir) file)
-;      (file-in-subdirs? (dir-dirs dir) file)))
-;
-;(check-expect (find? TS "read!")
-;              #true)
-;(check-expect (find? TS "part4")
-;              #false)
-;(check-expect (find? TS "hang")
-;              #true)
-;
-;; [List-of File] String -> Boolean
-;; Does file occur in lof of the current working directory?
-;(define (file-in-current-dir? lof file)
-;  (ormap (lambda (x) (string=? file (file-name x))) lof))
-;
-;(check-expect (file-in-current-dir? '() "part1")
-;              #false)
-;(check-expect (file-in-current-dir?
-;               (list
-;                (make-file "part1" 0 (make-date 2021 3 13 7 48 40) "")
-;                (make-file "part2" 0 (make-date 2021 3 13 7 48 45) "")
-;                (make-file "part3" 0 (make-date 2021 3 13 7 48 49) ""))
-;               "part4")
-;              #false)
-;(check-expect (file-in-current-dir?
-;               (list
-;                (make-file "part1" 0 (make-date 2021 3 13 7 48 40) "")
-;                (make-file "part2" 0 (make-date 2021 3 13 7 48 45) "")
-;                (make-file "part3" 0 (make-date 2021 3 13 7 48 49) ""))
-;               "part2")
-;              #true)
-;
-;; [List-of Dir] String -> Boolean
-;; Does file occur in any of the
-;; directories listed in lod?
-;(define (file-in-subdirs? lod file)
-;  (ormap (lambda (x) (find? x file)) lod))
-;
-;(check-expect (file-in-subdirs? '() "draw")
-;              #false)
-;(check-expect (file-in-subdirs? (dir-dirs TS) "draw")
-;              #true)
-;(check-expect (file-in-subdirs? (dir-dirs (first (dir-dirs TS))) "draw")
-;              #true)
-;(check-expect (file-in-subdirs? (dir-dirs (first (dir-dirs TS))) "read!")
-;              #true)
-;(check-expect (file-in-subdirs? (dir-dirs TS) "part4")
-;              #false)
-;              
