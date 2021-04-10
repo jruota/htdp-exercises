52ba318473749f2f0498136aaa91cb39af04e14b
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-04-10 15:15:30 +0200

    Exercise 342 - add challenge
    Designed the function find-all.

--- IV-Intertwined-Data/ex342.rkt
+++ IV-Intertwined-Data/ex342.rkt
@@ -13,9 +13,13 @@
 ; A Dir is a structure: 
 ;   (make-dir String [List-of Dir] [List-of File])
 
+; A Path is [List-of String].
+; Interpretation:
+;     Directions into a directory tree.
+
 ; A PathOrFalse is one of:
 ; – #false
-; – [List-of String]
+; – Path
 ; Interpretation:
 ;     The path to a file if it exists or #false.
 
@@ -31,6 +35,11 @@
   (create-dir
    (string-append "/home/jruota/learning/cs/teach-yourself-cs/"
                   "01-HtDP/htdp-exercises/IV-Intertwined-Data/TS")))
+
+(define TS-2
+  (create-dir
+   (string-append "/home/jruota/learning/cs/teach-yourself-cs/"
+                  "01-HtDP/htdp-exercises/IV-Intertwined-Data/TS-2")))
 
 ; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -94,6 +103,69 @@
               (list "TS" "read!"))
 (check-expect (find (first (dir-dirs TS)) "read!")
               (list "Libs" "Docs" "read!"))
+
+; Dir String -> [List-of Path]
+; Produce the list of all paths
+; that lead to f in dir.
+; If there is no file f, return
+; an empty list.
+(define (find-all dir0 f)
+  (local (; String [List-of [List-of String]] -> [List-of [List-of String]]
+          ; Prepend all elements of los with s,
+          ; i.e. prepend all Paths with the
+          ; parent directory.
+          (define (prepend s los)
+            (map (lambda (p) (cons s p)) los))
+
+          ; [List-of File] -> Boolean
+          ; Is file f among the files
+          ; in lof?
+          (define (lof-find? lof)
+            (ormap (lambda (x) (string=? (file-name x) f)) lof))
+
+          ; [List-of Dir] -> [List-of [List-of Path]]
+          ; If there is one or more files f
+          ; in the directory trees in lod,
+          ; return the paths to the file(s).
+          (define (lod-find lod)
+            (cond
+              [(empty? lod) '()]
+              [else
+               ; append reduces lists of empty lists to one empty list
+               (append (dir-find (first lod))
+                       (lod-find (rest lod)))]))
+
+          ; Dir -> [List-of [List-of Path]]
+          ; If there is one or more files f
+          ; in the directory tree dir1, return
+          ; the paths to the file(s).
+          (define (dir-find dir1)
+            (cond
+              [(lof-find? (dir-files dir1))
+               (cons (list (dir-name dir1) f)
+                     (prepend (dir-name dir1)
+                              (lod-find (dir-dirs dir1))))]
+              [else
+               (prepend (dir-name dir1)
+                        (lod-find (dir-dirs dir1)))])))
+    ; – IN –
+    (dir-find dir0)))
+
+(check-expect (find-all TS-2 "part4")
+              '())
+(check-expect (find-all TS-2 "part3")
+              (list (list "TS-2" "Text" "part3")))
+(check-expect (find-all (second (dir-dirs TS)) "part4")
+              '())
+(check-expect (find-all TS "part4")
+              '())
+(check-expect (find-all TS "part3")
+              (list (list "TS" "Text" "part3")))
+(check-expect (find-all TS "draw")
+              (list (list "TS" "Libs" "Code" "draw")))
+(check-expect (find-all TS "read!")
+              (list (list "TS" "read!")
+                    (list "TS" "Libs" "Docs" "read!")))
 
 ; from ex399.rkt +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
