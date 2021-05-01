0c9c4dd726672e164e46a479ef6b6229c29d12de
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-05-01 16:31:04 +0200

    Exercise 355 – redesign eval-var-lookup function
    Completely redesigned eval-var-lookup function
    according to the design recipe.

--- IV-Intertwined-Data/ex355.rkt
+++ IV-Intertwined-Data/ex355.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex354) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex355) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ; A BSL-var-expr is one of: 
@@ -35,25 +35,56 @@
 ; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ; BSL-var-expr AL -> Number
-; Starting from ex, apply subst to all
-; associations in da. If numeric? holds
-; for the result, determine its value;
-; otherwise signal an error.
+; Traverse ex and replace all symbols
+; with their respective values as specified
+; by da. If there is not corresponding entry
+; in da, signal an error.
 (define (eval-var-lookup ex da)
-  (local ((define SorF (assq ex da)))
+  (local (; BSL-var-expr -> Number
+          ; See purpose statement for eval-var-lookup.
+          (define (main ex1)
+            (cond
+              [(number? ex1) ex1]
+              [(symbol? ex1) (symbol-lookup ex1)]
+              [(add? ex1) (add-lookup ex1)]
+              [(mul? ex1) (mul-lookup ex1)]))
+
+          ; Symbol -> Number
+          ; Replace s with its corresponding
+          ; value from da. If there is no such
+          ; entry in da, signal an error.
+          (define (symbol-lookup s)
+            (local ((define AorF (assq s da)))
+              ; – IN –
+              (if (boolean? AorF)
+                  (error WRONG)
+                  (second AorF))))
+
+          ; Add -> Number
+          ; Return the value of a.
+          (define (add-lookup a)
+            (+ (main (add-left a))
+               (main (add-right a))))
+
+          ; Mul -> Number
+          ; Return the value of m.
+          (define (mul-lookup m)
+            (* (main (mul-left m))
+               (main (mul-right m)))))
     ; – IN –
-    (cond
-      [(number? ex) ex]
-      [(symbol? ex)
-       (if (boolean? (assq ex da))
-           (error WRONG)
-           (second (assq ex da)))]
-      [(add? ex)
-       (+ (eval-var-lookup (add-left ex) da)
-          (eval-var-lookup (add-right ex) da))]
-      [(mul? ex)
-       (* (eval-var-lookup (mul-left ex) da)
-          (eval-var-lookup (mul-right ex) da))])))
+    (main ex)))
+;    (cond
+;      [(number? ex) ex]
+;      [(symbol? ex)
+;       (if (boolean? (assq ex da))
+;           (error WRONG)
+;           (second (assq ex da)))]
+;      [(add? ex)
+;       (+ (eval-var-lookup (add-left ex) da)
+;          (eval-var-lookup (add-right ex) da))]
+;      [(mul? ex)
+;       (* (eval-var-lookup (mul-left ex) da)
+;          (eval-var-lookup (mul-right ex) da))])))
 
 (check-expect (eval-var-lookup
                (make-mul 3 (make-add 4 (make-mul 5 (make-add 6 7))))
