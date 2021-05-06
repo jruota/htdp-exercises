81ea3a24ea17e35d7eafea084c105bf3c0c77f92
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-05-06 21:01:03 +0200

    Exercise 357 – use updated version of subst function

--- IV-Intertwined-Data/ex357.rkt
+++ IV-Intertwined-Data/ex357.rkt
@@ -131,17 +131,40 @@
 ; from ex352.rkt ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ; BSL-var-expr Symbol Number -> BSL-var-expr
-; Replace all occurences of x in ex with v.
+; Replace all occurrences of x in ex with v.
 (define (subst ex x v)
-  (cond
-    [(number? ex) ex]
-    [(symbol? ex)
-     (if (symbol=? ex x)
-         v
-         ex)]
-    [(add? ex)
-     (make-add (subst (add-left ex) x v)
-               (subst (add-right ex) x v))]
-    [(mul? ex)
-     (make-mul (subst (mul-left ex) x v)
-               (subst (mul-right ex) x v))]))
+  (local (; BSL-var-expr -> BSL-var-expr
+          ; Replace all occurrences of x in ex0 with v.
+          (define (main ex0)
+            (cond
+              [(number? ex0) (subst-number ex0)]
+              [(symbol? ex0) (subst-symbol ex0)]
+              [(add? ex0) (subst-add ex0)]
+              [(mul? ex0) (subst-mul ex0)]))
+
+          ; Number -> Number
+          ; Return n.
+          (define (subst-number n)
+            n)
+
+          ; Symbol -> [Number or Symbol]
+          ; Replace s with v if s is equal
+          ; to x, otherwise return s.
+          (define (subst-symbol s)
+            (if (symbol=? s x)
+                v
+                s))
+
+          ; Add -> Add
+          ; Replace all occurrences of x in a with v.
+          (define (subst-add a)
+            (make-add (main (add-left a))
+                      (main (add-right a))))
+
+          ; Mul -> Mul
+          ; Replace all occurrences of x in m with v.
+          (define (subst-mul m)
+            (make-mul (main (mul-left m))
+                      (main (mul-right m)))))
+    ; – IN –
+    (main ex)))
