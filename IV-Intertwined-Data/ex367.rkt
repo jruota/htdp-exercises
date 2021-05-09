0d00db5b9c6e8760633d3334e2d9136e803d2414
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-05-09 17:22:41 +0200

    Exercise 367

---
+++ IV-Intertwined-Data/ex367.rkt
@@ -0,0 +1,28 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex367) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; An Xexpr is a list: 
+; – (cons Symbol Body)
+; – (cons Symbol (cons [List-of Attribute] Body))
+; where Body is short for [List-of Xexpr]
+
+; An Attribute is a list of two items:
+;   (cons Symbol (cons String '()))
+
+; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; Xexpr -> [List-of Attribute]
+; Retrieve the list of attributes of xe.
+(define (xexpr-attr xe)
+  (local ((define optional-loa+content (rest xe)))
+    (cond
+      [(empty? optional-loa+content) ...]
+      [else (... (first optional-loa+content)
+             ... (xexpr-attr (rest optional-loa+content)) ...)])))
+
+; The finished parsing function does not cotain a self-reference, because its
+; purpose is to find the attributes of the "parent" Xexpr. Therefore there is
+; no need to deal with the "child" Xexpr'essions contained in the "parent" Xexpr
+; and not self-reference.
