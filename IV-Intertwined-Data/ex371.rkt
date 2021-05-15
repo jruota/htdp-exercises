30c5766abb3e756a9702ed09ed1bca1b945a3d92
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-05-15 11:49:11 +0200

    Exercise 371

---
+++ IV-Intertwined-Data/ex371.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex371) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; An Xexpr is a list: 
+; – (cons Symbol Body)
+; – (cons Symbol (cons [List-of Attribute] Body))
+; – (cons Symbol (cons XWord Body))
+; – (cons Symbol (cons [List-of Attribute] (cons Xword Body)))
+; where Body is short for [List-of Xexpr].
+
+; An Attribute is a list of two items:
+;   (cons Symbol (cons String '()))
+
+; An XWord is '(word ((text String))).
+
+(check-expect '(word ((text "word")))
+              (list 'word (list (list 'text "word"))))
+(check-expect '(word ((text "word")))
+              (cons 'word (cons (cons (cons 'text (cons "word" '())) '()) '())))
