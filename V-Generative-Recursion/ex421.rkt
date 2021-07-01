5c5e81c70d7f8c25ced42040ae2781f24043bdbc
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-07-01 15:44:40 +0200

    Exercise 421

---
+++ V-Generative-Recursion/ex421.rkt
@@ -0,0 +1,35 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex421) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; [List-of 1String] N -> [List-of String]
+; bundles chunks of s into strings of length n
+; idea take n items and drop n at a time
+(define (bundle s n)
+  (cond
+    [(empty? s) '()]
+    [else
+     (cons (implode (take s n)) (bundle (drop s n) n))]))
+ 
+; [List-of X] N -> [List-of X]
+; keeps the first n items from l if possible or everything
+(define (take l n)
+  (cond
+    [(zero? n) '()]
+    [(empty? l) '()]
+    [else (cons (first l) (take (rest l) (sub1 n)))]))
+ 
+; [List-of X] N -> [List-of X]
+; removes the first n items from l if possible or everything
+(define (drop l n)
+  (cond
+    [(zero? n) l]
+    [(empty? l) l]
+    [else (drop (rest l) (sub1 n))]))
+
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; (bundle '("a" "b" "c") 0)
+
+; (bundle '("a" "b" "c") 0) creates an infinite loop, since drop never drops any
+; items and bundle is given the same arguments over and over again. Thus the
+; result would be an infinitely long list of empty lists.
