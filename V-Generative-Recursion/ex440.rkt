1aba6cdf738fbb9a3167ae6218eff02e57a60026
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-07-04 07:35:57 +0200

    Exercise 440

---
+++ V-Generative-Recursion/ex440.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex440) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; N[>= 1] N[>= 1] -> N
+; finds the greatest common divisor of n and m
+(define (gcd-generative n m)
+  (local (; N[>= 1] N[>=1] -> N
+          ; generative recursion
+          ; (gcd L S) == (gcd S (remainder L S)) 
+          (define (clever-gcd L S)
+            (cond
+              [(= S 0) L]
+              [else (clever-gcd S (remainder L S))])))
+    (clever-gcd (max m n) (min m n))))
+
+(check-expect (gcd-generative 6 25) 1)
+(check-expect (gcd-generative 18 24) 6)
+
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(time (gcd-generative 101135853 45014640))
