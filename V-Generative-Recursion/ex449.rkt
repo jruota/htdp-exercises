2521f5e0aa475ba17819f68c36cea5f590afab37
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-08-21 11:37:39 +0200

    Exercise 449

---
+++ V-Generative-Recursion/ex449.rkt
@@ -0,0 +1,77 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex449) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; DATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(define ε 0.000000001)
+
+; FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; [Number -> Number] Number Number -> Number
+; determines R such that f has a root in [R,(+ R ε)]
+; assume f is continuous 
+; assume (or (<= (f left) 0 (f right)) (<= (f right) 0 (f left)))
+; generative divides interval in half, the root is in one of the two
+; halves, picks according to assumption 
+(define (find-root f left right)
+  (cond
+    [(<= (- right left) ε) left]
+    [else
+     (local ((define mid (/ (+ left right) 2))
+             (define f@mid (f mid))
+             (define f@left (f left))
+             (define f@right (f right))
+
+             ; Number Number Number Number -> Number
+             ; Helper function to find-root
+             ; that avoids recomputations.
+             (define (root-helper l r f@l f@r)
+               (cond
+                 [(<= (- r l) ε) l]
+                 [else
+                  (local ((define m (/ (+ l r) 2))
+                          (define f@m (f mid)))
+                    ; – IN –
+                    (cond
+                      [(or (<= f@l 0 f@m) (<= f@m 0 f@l))
+                       (root-helper l m f@l f@m)]
+                      [(or (<= f@m 0 f@r) (<= f@r 0 f@m))
+                       (root-helper m r f@m f@r)]))])))
+       ; – IN –
+       (cond
+         [(or (<= f@left 0 f@mid) (<= f@mid 0 f@left))
+          (root-helper left mid f@left f@mid)]
+         [(or (<= f@mid 0 f@right) (<= f@right 0 f@mid))
+          (root-helper mid right f@mid f@right)]))]))
+
+(check-satisfied (find-root poly 1 3) (range-checker 1 3))
+(check-satisfied (find-root poly 3 19) (range-checker 3 19))
+
+(check-satisfied (find-root poly -15 3) (range-checker 1 3))
+
+(check-expect (find-root poly 1.9999999999 2.0000000001)
+              1.9999999999)
+
+; Number -> Number
+(define (poly x)
+  (* (- x 2) (- x 4)))
+
+; Number Number -> [Number -> Boolean]
+; Create a predicate that checks whether a number
+; is within the range [a, b], where a <= b.
+(define (range-checker a b)
+  (local (; Is x within the range [a, b]?
+          ; Number -> Boolean
+          (define (within-range? x)
+            (and (<= a x) (<= x b))))
+    ; – IN –
+    within-range?))
+
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+; How many recomputations of (f left) does this design maximally avoid?
+
+; The version in figure 159 calcutales (f left) / (f right) twice per call while
+; the new design calculates them only once.
+; Additionally there is no recomputation of a boundary across recursive calls.
+; Ultimately, this design avoids two recomputations.
