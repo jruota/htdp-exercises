fbc365f67f05424b32d996d6873920f074b166e5
Jaroław Kołosowski <jarokolosowski@gmail.com>
2021-12-12 15:49:56 +0100

    Exercise 496

---
+++ VI-Accumulators/ex496.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex496) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Since
+
+    ; a is the product of the natural numbers in the interval [n0,n)
+
+; the value for a when n0 = 10 and n = 8 should be
+
+(* 10 9)
+
+; as a is the result of an open interval.
