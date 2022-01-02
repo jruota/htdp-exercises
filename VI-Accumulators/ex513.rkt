5efbd9fe79ea1c57df1af09e6b2a7a4a25edf79b
Jaroław Kołosowski <jarokolosowski@gmail.com>
2022-01-02 18:39:22 +0100

    Exercise 513 – minor addition to data definition
    Added the (list Lam Lam) part to the dedfinition of Lam.
    Removed the (require 2htdp/image) part, as there are
    no tests using empty-image.

--- VI-Accumulators/ex513.rkt
+++ VI-Accumulators/ex513.rkt
@@ -1,12 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex513) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(require 2htdp/image)
-
 ; DATA DEFINITIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ; A Lam is one of: 
 ; – a Symbol
+; – (list Lam Lam)
 ; – (make-λ-fun Symbol Lam)
 ; – (make-λ-app make-λ-fun Lam)
 
